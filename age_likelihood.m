227d493248e49f80136c07bc5cb2a0d4fd21e851
Durban G Keeler <durban.keeler@gmail.com>
2018-02-19 15:45:22 -0700

    Combined peak and RT likelihoods and parallelized the code

--- age_likelihood.m
+++ age_likelihood.m
@@ -1,5 +1,5 @@
-% This is an alpha script to combine the peak likelihood estimates and 
-% radon transform estimates to produce a single age-depth profile 
+% This is an alpha script to combine the peak likelihood estimates and
+% radon transform estimates to produce a single age-depth profile
 % distribution
 
 % Assigns the path to the data and add-on folders, based on whether running
@@ -26,7 +26,7 @@ addpath(genpath(addon_folder))
 %%
 % Directory to radar files of interest
 radar_dir = strcat(data_path, ['SEAT_Traverses' filesep 'SEAT2010Kuband'...
-    filesep 'ProcessedSEAT2010' filesep 'grid_SEAT10_6' filesep]);
+    filesep 'ProcessedSEAT2010' filesep 'grid_SEAT10_3' filesep]);
 
 % List all files matching 'wild' within radar directory
 wild = 'layers*';
@@ -94,7 +94,7 @@ stats = regionprops(CC, 'PixelList');
 % Define variables for the likelihood logistic function
 Po = 0.01;
 K = 1;
-P_50 = 500;
+P_50 = 750;
 r = log((K*Po/0.50-Po)/(K-Po))/-P_50;
 
 % Preallocate weight matrix and stats structure
@@ -142,11 +142,13 @@ P = extractfield(stats_new, 'Likelihood');
 figure
 plot(lengths, P, 'o')
 
-% % Caculate radon-transform weighting coefficient for radar file based on 
-% % average segment length
-w_RT = mean(layer_ima(layer_ima>0));
+% Caculate radon-transform weighting coefficient for radar file based on
+% average segment length
+% w_RT = mean(layer_ima(layer_ima>0));
+w_RT = K*Po./(Po + (K-Po)*exp(-r*mean(lengths)));
 
 %%
+
 % Year associated with the first pick of the algorithm
 % age_top = round(radar.collect_date);
 age_top = radar.collect_date;
@@ -162,112 +164,31 @@ depth_btw_yr = [depth_diff; mean(depth_diff)];
 % Model of distance between annual horizions with depth in core
 f = polyfit(core.depth(yr_idx), depth_btw_yr, 1);
 
-% Expected thickness (and standard deviation) of annual layers 
+% Expected thickness (and standard deviation) of annual layers
 % throughout the depth of the core
 dist_Ex = f(1)*radar.depth_interp + f(2);
 dist_STD = std(depth_btw_yr);
 
 
-%% PURE TESTING
-i = 100;
-likelihood_peaks = zeros(size(radar.data_smooth, 1), 1);
-
-% Estimate the depth of annual peaks within each smoothed radar trace
-data_i = radar.data_smooth(:,i);
-depth_i = radar.depth_interp;
-minProm = 0.01;
-minProm = 0.1*iqr(data_i);
-[~, idx_peaks, ~, Prom] = findpeaks(data_i, 'MinPeakProminence', minProm);
-
-% Solve for r so that prominence with 0.50 probability is equal to
-% interquartile range of the smoothed data (may need to be optimized to
-% observations using a scaling constant)
-Po = 0.01;
-K = 1;
-P_50 = 1*iqr(data_i);
-r = log((K*Po/0.50-Po)/(K-Po))/-P_50;
-
-% Probability of peak representing a year based on a logistic function
-% with rate (r) calculated above
-P_peaks = K*Po./(Po + (K-Po)*exp(-r*Prom));
-likelihood_peaks(idx_peaks) = P_peaks;
-
-% RT likelihoods
-%depth_RT = depth_i(layer_ima(:,i)>0);
-likelihood_RT = layer_ima(:,i);
-
-% Diagnostic figure
-figure
-plot(depth_i, likelihood_peaks, 'LineWidth', 2)
-hold on
-plot(depth_i, likelihood_RT)
-hold off
-
-%%Compare age-depth profiles from the two independent methods
-depths_peaks = depth_i(idx_peaks);
-
-yr_idx = zeros(length(depths_peaks), Ndraw);
-for j = 1:length(depths_peaks)
-    R = rand(Ndraw, 1) <= P_peaks(j);
-    yr_idx(j,:) = R;
-end
-
-age_peaks = zeros(length(data_i), Ndraw);
-for j = 1:Ndraw
-    depths_j = [0; depths_peaks(logical(yr_idx(:,j)))];
-    yrs_j = ([age_top yr_pick1:-1:yr_pick1-length(depths_j)+2])';
-    age_peaks(:,j) = interp1(depths_j, yrs_j, depth_i, 'linear', 'extrap');
-end
-
-
-depths_RT = depth_i(layer_ima(:,i)>0);
-P_RT = layer_ima(:,i);
-P_RT = P_RT(P_RT>0);
-
-yr_idx = zeros(length(depths_RT), Ndraw);
-for j = 1:length(depths_RT)
-    R = rand(Ndraw, 1) <= P_RT(j);
-    yr_idx(j,:) = R;
-end
-
-age_RT = zeros(length(data_i), Ndraw);
-for j = 1:Ndraw
-    depths_j = [0; depths_RT(logical(yr_idx(:,j)))];
-    yrs_j = ([age_top yr_pick1:-1:yr_pick1-length(depths_j)+2])';
-    age_RT(:,j) = interp1(depths_j, yrs_j, depth_i, 'linear', 'extrap');
-end
-
-figure
-hold on
-plot(depth_i, mean(age_peaks, 2))
-plot(depth_i, mean(age_RT, 2))
-hold off
-    
-
-
-
-
-%%
-
-
-% Estimate the depth of annual peaks within each smoothed radar trace
-ages = zeros([size(radar.data_smooth) Ndraw]);
+%% Calculate RT and peak likelihoods separately
+age_peaks = zeros([size(radar.data_smooth) Ndraw]);
+age_RT = zeros([size(radar.data_smooth) Ndraw]);
+age_weighted = zeros([size(radar.data_smooth) Ndraw]);
 data = radar.data_smooth;
 depth_i = radar.depth_interp;
 parfor i = 1:size(radar.data_smooth, 2)
+    
+    %%Calculate RT likelihoods
+    likelihood_RT = layer_ima(:,i);
+    
+    %%Calculate peak likelihoods
     data_i = data(:,i);
-    minProm = 0.01;
-    minDist = abs(min(dist_Ex) - 3*dist_STD);
-    minDist = 0;
-    [~, depths_peaks, ~, Prom] = findpeaks(data_i, depth_i, ...
-        'MinPeakProminence', minProm, 'MinPeakDistance', minDist);
+    likelihood_peaks = zeros(length(data_i), 1);
+    % minProm = 0.01;
+    minProm = 0.1*iqr(data_i);
+    [~, idx_peaks, ~, Prom] = findpeaks(data_i, 'MinPeakProminence', minProm);
     
-    % Remove the surface horizon peak from the data
-    [~,max_idx] = max(Prom);
-    Prom(max_idx) = [];
-    depths_peaks(max_idx) = [];
-    
-    % Solve for r so that prominence with 0.50 probability is equal to 
+    % Solve for r so that prominence with 0.50 probability is equal to
     % interquartile range of the smoothed data (may need to be optimized to
     % observations using a scaling constant)
     Po = 0.01;
@@ -275,42 +196,123 @@ parfor i = 1:size(radar.data_smooth, 2)
     P_50 = 1*iqr(data_i);
     r = log((K*Po/0.50-Po)/(K-Po))/-P_50;
     
+    % % Calculate distance weights based off of the distances to the nearest
+    % % four peaks (in future should scale the effect based off the variance
+    % % in the expected distance, dist_Ex_i)
+    % [~, Prom_dist] = knnsearch(depths_peaks, depths_peaks, 'K', 5);
+    % dist_Ex_i = f(1)*depths_peaks + f(2);
+    % w = sum([0.5 0.5 0.25 0.25].*(Prom_dist(:,2:end)-dist_Ex_i), 2)./dist_Ex_i;
+    % Pw = P.^(1-w);
+    % P = Pw;
+    
     % Probability of peak representing a year based on a logistic function
     % with rate (r) calculated above
-    P = K*Po./(Po + (K-Po)*exp(-r*Prom));
+    P_peaks = K*Po./(Po + (K-Po)*exp(-r*Prom));
+    likelihood_peaks(idx_peaks) = P_peaks;
     
-%     % Function using a more general logistic function (requires
-%     % optimization of k)
-%     L = 1;
-%     P_50 = mean(Prom);
-%     k = 4;
-%     P = L./(1 + exp(-k.*(Prom-P_50)));
-
-    % Calculate distance weights based off of the distances to the nearest
-    % four peaks (in future should scale the effect based off the variance 
-    % in the expected distance, dist_Ex_i)
-    [~, Prom_dist] = knnsearch(depths_peaks, depths_peaks, 'K', 5);
-    dist_Ex_i = f(1)*depths_peaks + f(2);
-    w = sum([0.5 0.5 0.25 0.25].*(Prom_dist(:,2:end)-dist_Ex_i), 2)./dist_Ex_i;
-    Pw = P.^(1-w);
-    P = Pw;
+    %%%Compare age-depth profiles from each method and the weighted average
     
+    %%Calculate age-depth profile distribution from peak likelihoods
+    
+    depths_peaks = depth_i(idx_peaks);
     yr_idx = zeros(length(depths_peaks), Ndraw);
     for j = 1:length(depths_peaks)
-        R = rand(Ndraw, 1) <= P(j);
+        R = rand(Ndraw, 1) <= P_peaks(j);
         yr_idx(j,:) = R;
     end
-    
     age_i = zeros(length(data_i), Ndraw);
     for j = 1:Ndraw
         depths_j = [0; depths_peaks(logical(yr_idx(:,j)))];
         yrs_j = ([age_top yr_pick1:-1:yr_pick1-length(depths_j)+2])';
+        %     age_peaks(:,j) = interp1(depths_j, yrs_j, depth_i, 'linear', 'extrap');
         age_i(:,j) = interp1(depths_j, yrs_j, depth_i, 'linear', 'extrap');
     end
-    ages(:,i,:) = age_i;
+    age_peaks(:,i,:) = age_i;
+    
+    %%Calculate age-depth profile distribution from RT likelihoods
+    
+    depths_RT = depth_i(layer_ima(:,i)>0);
+    P_RT = layer_ima(:,i);
+    P_RT = P_RT(P_RT>0);
+    yr_idx = zeros(length(depths_RT), Ndraw);
+    for j = 1:length(depths_RT)
+        R = rand(Ndraw, 1) <= P_RT(j);
+        yr_idx(j,:) = R;
+    end
+    age_i = zeros(length(data_i), Ndraw);
+    for j = 1:Ndraw
+        depths_j = [0; depths_RT(logical(yr_idx(:,j)))];
+        yrs_j = ([age_top yr_pick1:-1:yr_pick1-length(depths_j)+2])';
+        age_i(:,j) = interp1(depths_j, yrs_j, depth_i, 'linear', 'extrap');
+    end
+    age_RT(:,i,:) = age_i;
+    
+    %%Shift RT likelihood depths to match nearby depths in peak likelihood
+    
+    tol = 3;
+    idx_RT = find(layer_ima(:,i));
+    RT_shift = zeros(length(likelihood_RT), 1);
+    for j = idx_RT'
+        if j<=tol
+            data_j = likelihood_peaks(1:j+tol);
+        elseif j+tol>length(likelihood_peaks)
+            data_j = likelihood_peaks(j-tol:end);
+        else
+            data_j = likelihood_peaks(j-tol:j+tol);
+        end
+        if isempty(data_j(data_j>0))
+            RT_shift(j) = likelihood_RT(j);
+        else
+            data_j(data_j<=0) = -1;
+            [~, min_idx] = min(abs(data_j - likelihood_RT(j)));
+            RT_shift(j-tol+min_idx-1) = likelihood_RT(j);
+        end
+    end
+    
+    %%Calculate age-depth profile distribution from weighted likelihoods
+    
+    % Combine two likelihoods into a single weighted average
+    likelihood_w = (likelihood_peaks + w_RT*RT_shift)/(1+w_RT);
+    
+    depths_weighted = depth_i(likelihood_w>0);
+    P_w = likelihood_w(likelihood_w>0);
+    yr_idx = zeros(length(depths_weighted), Ndraw);
+    for j = 1:length(depths_weighted)
+        R = rand(Ndraw, 1) <= P_w(j);
+        yr_idx(j,:) = R;
+    end
+    age_i = zeros(length(data_i), Ndraw);
+    for j = 1:Ndraw
+        depths_j = [0; depths_weighted(logical(yr_idx(:,j)))];
+        yrs_j = ([age_top yr_pick1:-1:yr_pick1-length(depths_j)+2])';
+        age_i(:,j) = interp1(depths_j, yrs_j, depth_i, 'linear', 'extrap');
+    end
+    age_w(:,i,:) = age_i;
 end
-radar.age = ages;
 
-radar = rmfield(radar, {'time_trace', 'arr_layers', 'TWTT'});
+% Diagnostic figure
+figure
+hold on
+h1 = plot(radar.depth_interp, mean(mean(age_peaks, 3), 2), 'm');
+h2 = plot(radar.depth_interp, mean(mean(age_RT, 3), 2), 'c');
+h3 = plot(radar.depth_interp, mean(mean(age_w, 3), 2), 'r');
+plot(radar.depth_interp, mean(mean(age_w, 3), 2)+std(mean(age_w, 3), [], 2), 'r--')
+plot(radar.depth_interp, mean(mean(age_w, 3), 2)-std(mean(age_w, 3), [], 2), 'r--')
+h4 = plot(core.depth, core.age, 'b');
+legend([h1 h2 h3 h4], 'Mean peak age', 'Mean RT age', 'Mean weighted age', 'Core age')
+hold off
 
-
+% % Diagnostic figure
+% figure
+% hold on
+% plot(depth_i, mean(age_peaks, 2), 'b')
+% plot(depth_i, mean(age_peaks, 2) + std(age_peaks, [], 2), 'b--')
+% plot(depth_i, mean(age_peaks, 2) - std(age_peaks, [], 2), 'b--')
+% plot(depth_i, mean(age_RT, 2), 'r')
+% plot(depth_i, mean(age_RT, 2) + std(age_RT, [], 2), 'r--')
+% plot(depth_i, mean(age_RT, 2) - std(age_RT, [], 2), 'r--')
+% plot(depth_i, mean(age_w, 2), 'm', 'LineWidth', 2)
+% plot(depth_i, mean(age_w, 2) + std(age_w, [], 2), 'm--')
+% plot(depth_i, mean(age_w, 2) - std(age_w, [], 2), 'm--')
+% plot(core.depth, core.age, 'k', 'LineWidth', 2)
+% hold off
