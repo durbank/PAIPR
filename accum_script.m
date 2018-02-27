356e9cf69e979a0dabf054de752c7ea4f1afebf0
Durban G Keeler <durban.keeler@gmail.com>
2018-02-27 10:59:45 -0700

    Added scripts for calculating accumulation from the output of the age-depth profile scripts

---
+++ accum_script.m
@@ -0,0 +1,54 @@
+% Directory to radar files of interest
+
+PC_true = ispc;
+switch PC_true
+    case true
+        data_path = 'E:/Research/Antarctica/WAIS Variability/';
+        addon_path = 'E:/Research/Antarctica/WAIS Variability/Addons/';
+    case false
+        data_path = '/Volumes/WARP/Research/Antarctica/WAIS Variability/';
+        addon_path = '/Users/Durbank/Documents/MATLAB/Add-Ons/';
+end
+
+% Addons needed for analysis
+% Add Antarctic Mapping Toolbox (AMT) to path
+addon_folder = strcat(addon_path, 'AntarcticMappingTools_v5.03/');
+addpath(genpath(addon_folder))
+
+% Import firn core data
+[cores] = import_cores(strcat(data_path, ['SEAT_cores' filesep ...
+    'DGK_core_data.xlsx']));
+
+%%
+
+radar_dir = strcat(data_path, ['SEAT_Traverses' filesep 'SEAT2010Kuband'...
+    filesep 'ProcessedSEAT2010' filesep 'grid_SEAT10_4' filesep]);
+
+% List all files matching 'wild' within radar directory
+wild = 'layers*';
+files = dir(strcat(radar_dir, wild));
+
+% Select individual radar file for analysis
+i = randi(length(files));
+file = strcat([files(i).folder filesep], files(i).name);
+
+% Number of simulations to perform on age-depth Monte Carlo
+Ndraw = 100;
+
+% Calculate radar ages and associated other data
+[radar, core] = age_MC(file, cores, Ndraw);
+
+% Calculate annual accumulation rates from data
+[SMB] = calc_SWE(radar, core);
+
+% Diagnostic figure
+figure
+hold on
+plot(SMB.core_yr, SMB.core_accum, 'k--')
+plot(SMB.core_yr, movmean(SMB.core_accum, 3), 'b', 'LineWidth', 2)
+plot(SMB.radar_yr, mean(SMB.radar_accum, 2), 'r', 'LineWidth', 2)
+plot(SMB.radar_yr, mean(SMB.radar_accum, 2) + std(SMB.radar_accum, [], 2), 'r--')
+plot(SMB.radar_yr, mean(SMB.radar_accum, 2) - std(SMB.radar_accum, [], 2), 'r--')
+xlabel('Year C.E.')
+ylabel('Accumulation [mm w.e./a]')
+hold off
---
+++ calc_SWE.m
@@ -0,0 +1,96 @@
+function [SMB] = calc_SWE(radar, core)
+
+% Generate mean age-depth profiles for each trace from MC simulations
+ages = mean(radar.age, 3);
+
+% Define first year with complete accumulation data and last year with
+% robust accumulation data
+yr_top = floor(max(ages(1,:)));
+yr_end = ceil(min(ages(end,:)));
+
+% Generate density with depth model for radar data
+rho_ice = 0.917;
+rho_0 = mean(core.rho(1:10));
+rho_coeff = coeffvalues(radar.rho_fit);
+rho_mod = rho_coeff(1)*radar.depth_interp.^rho_coeff(2) + rho_coeff(3);
+% rho_mod = rho_ice - (rho_ice-rho_0)*exp(rho_coeff.*radar.depth_interp);
+
+% Generate variance model for density with depth, and calculate standard
+% deviation with depth for density model
+rho_res = rho_coeff(1)*core.depth.^rho_coeff(2) + rho_coeff(3);
+% rho_res = core.rho - (rho_ice - (rho_ice-rho_0)*exp(rho_coeff*core.depth));
+rho_var = movvar(rho_res, round(length(rho_res)/3));
+fun = @(a, x) (max(rho_var)-mean(rho_var(end-50:end)))./(a.^x) + ...
+    mean(rho_var(end-50:end));
+var_fit = fit(core.depth, rho_var, fun, 'StartPoint', 1.5);
+var_coeff = coeffvalues(var_fit);
+std_mod = 1000*sqrt(abs((max(rho_var)-mean(rho_var(end-50:end)))./...
+    (var_coeff.^radar.depth_interp) + mean(rho_var(end-50:end))));
+
+% % Calculate accumulation at each depth interval (0.02 m) with simulated
+% % noise
+noise = repmat(std_mod, 1, size(radar.age, 2)).*randn(size(radar.age));
+accum_dt = 0.02*(1000*repmat(rho_mod, 1, size(ages, 2)) + noise);
+% accum_dt = 0.02*(1000*repmat(rho_mod, 1, size(radar.age, 2), Ndraw) + noise);
+
+accum_yr = (yr_top:-1:yr_end)';
+accum = zeros(length(accum_yr), size(radar.age, 2));
+for i = 1:size(accum, 2)
+    years_i = ages(:,i);
+    yr_idx = logical([diff(floor(years_i)); 0]);
+    yr_loc = find(yr_idx);
+    if length(yr_loc) - 1 > length(accum_yr)
+        n_length = length(accum_yr);
+    else
+        n_length = length(yr_loc) - 1;
+    end
+    %         yr_all = round(years_i(yr_idx(:,j),j));
+    %         yr_num = yr_all(2:end);
+    % accum_yr(1:length(yr_num),i,j) = yr_num;
+    %         accum_j = zeros(length(yr_num), 1);
+    accum_i = zeros(size(accum_yr));
+    for n = 1:n_length
+        accum_i(n) = sum(accum_dt(yr_loc(n)+1:yr_loc(n+1),i));
+    end
+    %         accum(1:length(yr_num),i,j) = accum_j;
+    accum(:,i) = accum_i(1:length(accum_yr));
+    
+end
+
+% Truncate accum to years where all traces have data for all years
+accum_yr = accum_yr(all(accum, 2));
+if max(diff(accum_yr)) > 1
+    disp('Issue with accumulation year trunctation')
+end
+accum = accum(all(accum ,2),:);
+
+% Stack adjacent records based on the lateral averaging window size
+window = 50;
+stack_idx = 1:window:size(accum, 2);
+accum_stack = zeros(length(accum_yr), length(stack_idx)-1);
+std_stack = zeros(length(accum_yr), length(stack_idx)-1);
+Northing_stack = zeros(1, length(stack_idx)-1);
+Easting_stack = zeros(length(accum_yr), length(stack_idx)-1);
+for i = 1:length(stack_idx)-1
+    accum_stack(:,i) = mean(accum(:,stack_idx(i):stack_idx(i+1)), 2);
+    std_stack(:,i) = std(accum(:,stack_idx(i):stack_idx(i+1)), [], 2);
+    Northing_stack(i) = mean(radar.Northing(stack_idx(i):stack_idx(i+1)));
+    Easting_stack(i) = mean(radar.Easting(stack_idx(i):stack_idx(i+1)));
+end
+
+% Calculate annual accumulation for the weighted composite core
+core_accum_dt = 0.02*(1000*core.rho);
+core_yr_idx = logical([1; diff(floor(core.age))]);
+yr_loc = find(core_yr_idx);
+yr_all = round(core.age(yr_loc));
+core_yr = yr_all(2:end);
+core_accum = zeros(length(core_yr), 1);
+for n = 1:length(core_yr)
+    core_accum(n) = sum(core_accum_dt(yr_loc(n)+1:yr_loc(n+1)));
+end
+
+
+SMB = struct('Easting', Easting_stack, 'Northing', Northing_stack, ...
+    'radar_accum', accum_stack, 'radar_yr', accum_yr, 'radar_std', ...
+    std_stack, 'core_accum', core_accum, 'core_yr', core_yr);
+end
