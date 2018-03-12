493c11b68677453da46a762f0d3fb724098eae83
Durban G Keeler <durban.keeler@gmail.com>
2018-03-12 17:58:27 -0600

    Added data exploration plots to end of script

--- OIB_script.m
+++ OIB_script.m
@@ -39,9 +39,66 @@ Ndraw = 100;
 [cores] = import_cores(strcat(data_path, ['SEAT_cores' filesep ...
     'DGK_core_data.xlsx']));
 
-% Import and process OIB data
-[radar] = OIB_import(file);
+% Calculate radar ages and associated other data
+[radar, core] = age_MC(file, cores, Ndraw);
 
+%%
 
+% Select random radar trace for comparison plots
+i = randi(size(radar.data_smooth, 2));
+
+% Plot radargram
+figure('Position', [200 200 1500 800])
+imagesc([0 radar.dist(end)], [0 radar.depth_interp(end)], radar.data_smooth, [-2 2])
+colorbar
+xlabel('Distance along profile (m)')
+ylabel('Depth (m)')
+hold on
+plot([radar.dist(i) radar.dist(i)], [0 radar.depth_interp(end)], 'r', 'LineWidth', 2)
+xlim([0 radar.dist(end)])
+ylim([0 radar.depth_interp(end)])
+set(gca, 'Ydir', 'reverse', 'FontSize', 18)
+hold off
+
+% Compare radar signal at random trace to annual horizion selection at that
+% trace
+% layer_idx = logical([diff(floor(squeeze(radar.age(:,i,100)))); 0]);
+layer_idx = logical([diff(floor(squeeze(radar.age(:,i,randi(Ndraw))))); 0]);
+figure('Position', [50 50 500 1200])
+hold on
+plot(radar.data_smooth(:,i), radar.depth_interp, 'r', 'LineWidth', 1.5)
+scatter(radar.data_smooth(layer_idx,i), radar.depth_interp(layer_idx), 'b<', 'filled')
+xlim([-1.25 2])
+xlabel('Radar Z-score')
+ylabel('Depth (m)')
+set(gca, 'Ydir', 'reverse')
+hold off
+
+% Compare radar signal at random trace to the mean annual horizions at that
+% same random trace
+layer_idx = logical([diff(floor(mean(squeeze(radar.age(:,i,:)), 2))); 0]);
+figure('Position', [50 50 1200 500])
+hold on
+plot(radar.depth_interp, radar.data_smooth(:,i))
+plot([radar.depth_interp(layer_idx) radar.depth_interp(layer_idx)], ...
+    [-1.5 1.5], 'k');
+ylim([-1.5 1.5])
+
+% Plot core vs mean radar ages (and uncertainty) for random trace
+age_mean = mean(squeeze(radar.age(:,i,:)), 2);
+age_ERR = 2*std(squeeze(radar.age(:,i,:)), [], 2);
+
+figure
+hold on
+h1 = plot(core.depth, core.age, 'b', 'LineWidth', 2);
+h2 = plot(radar.depth_interp, age_mean, 'r', 'LineWidth', 2);
+h3 = plot(radar.depth_interp, age_mean+age_ERR, 'r--', 'LineWidth', 0.5);
+h4 = plot(radar.depth_interp, age_mean-age_ERR, 'r--', 'LineWidth', 0.5);
+xlabel('Depth (m)')
+ylabel('Calendar Year')
+ylim([min([min(core.age) min(age_mean-age_ERR)]) max([max(core.age) max(age_mean)])])
+legend([h1 h2], 'Core age (manual)', 'Radar age (automated)', 'Location', 'ne')
+set(gca, 'FontSize', 18)
+hold off
 
 
