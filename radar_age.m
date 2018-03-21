e5877f048013ca7285616f30b29b42433122b02f
Durban G Keeler <durban.keeler@gmail.com>
2018-03-21 14:30:04 -0600

    Changes to ensure that only one row per trace is assigned to each layer; modified local peak search width to automatically adjust to 100 m window; output of likelihood matrix to 'radar.likelihood' field; P_50 scaled by mean interquartile range of data

--- radar_age.m
+++ radar_age.m
@@ -43,7 +43,7 @@ radar.data_smooth = sgolayfilt(radar.data_stack, 3, 9);
 age_top = radar.collect_date;
 yr_pick1 = ceil(radar.collect_date - 1);
 
-%% Find the depth+prominence of peaks within each smoothed radar trace
+%% Find the depth and prominence of peaks within each smoothed radar trace
 
 % Preallocate arrays for various components
 peaks = zeros(size(radar.data_smooth));
@@ -61,8 +61,9 @@ for i = 1:size(radar.data_smooth, 2)
     minDist = 0.12;                 % Min distance between peaks (in meters)
     
     % Find peaks in each trace based on requirements
-    [~, peaks_idx_i, widths_i, Prom_i] = findpeaks(data_i, 'MinPeakProminence', ...
-        minProm, 'MinPeakDistance', minDist/resolution);
+    [~, peaks_idx_i, widths_i, Prom_i] = findpeaks(data_i, ...
+        'MinPeakProminence', minProm, ...
+        'MinPeakDistance', minDist/resolution);
 %     peaks_i = zeros(length(data_i), 1);
 %     peaks_i(peaks_idx_i) = Prom_i;
 %     peaks(:,i) = peaks_i;
@@ -81,8 +82,8 @@ end
 %%
 
 % Define size of ~quasi bin confidence interval
-err_bin = 6;
 err_bin = minDist/resolution;
+err_bin = 6;
 
 % Preallocate cell array for layer numbers and initialize values by
 % assigning unique layer numbers to each peak in the first trace
@@ -99,13 +100,14 @@ peak_group(depth_idx{1},1) = Groups{1};
 
 for i = 2:size(peaks, 2)
     
-    % Determine column bounds for the local search window for i based on 
-    % the bin error window
-    col_idx = [max([i-round(0.5*err_bin) 1]) i-1];
+    % Assign column bounds for the ith local search window based on 100 m
+    % window
+    col_idx = [max([i-round(100/mean(diff(radar.dist))) 1]) i-1];
+%     col_idx = [max([i-round(0.5*err_bin) 1]) i-1];
     
     for j = 1:length(Proms{i})
         
-        % Determine cell array index for j in ith trace
+        % Determine cell array index for jth peak in ith trace
         j_idx = depth_idx{i}(j);
         
         % Determine index values for the row boundaries of the local search
@@ -113,8 +115,6 @@ for i = 2:size(peaks, 2)
         % half-width of peak (i,j)
         row_idx = [max([j_idx-round(0.5*(err_bin+widths{i}(j))) 1]) ...
             min([j_idx+round(0.5*(err_bin+widths{i}(j))) size(peaks, 1)])];
-%         row_idx = [max([j_idx-round(0.5*widths{i}(j)) 1]) ...
-%             min([j_idx+round(0.5*widths{i}(j)) size(peaks, 1)])];
         
         % Define local window to search for matching layer numbers
         peaks_local = peaks(row_idx(1):row_idx(2),col_idx(1):col_idx(2));
@@ -132,7 +132,7 @@ for i = 2:size(peaks, 2)
             (Proms{i}(j) - peaks_local(local_idx)).^2);
         
         % Select the nearest neighbor to peak (i,j)
-        [dist_min, dist_idx] = min(w_dist);
+        [~, dist_idx] = min(w_dist);
         
         % (I may add a tolerance in the future)
         if ~isempty(dist_idx)     % if j_min <= bin_res/2
@@ -154,27 +154,58 @@ for i = 2:size(peaks, 2)
     end
 end
 
+% Preallocate arrays for the matrix indices of members of each layer and
+% integrated prominence-distance mapped to the radar matrix
 layers_idx = cell(1,new_group-1);
-layers_val = zeros(1, length(layers_idx));
 layer_peaks = zeros(size(peaks));
 for i = 1:length(layers_idx)
+    
+    % Find matrix indices of all members of ith layer, and assign to
+    % preallocated cell array
     layers_idx{i} = find(peak_group==i);
+    
+    % Find row and col indices of members of ith layer
+    [~, col] = ind2sub(size(radar.data_smooth), layers_idx{i});
+    
+    % If multiple rows exist for the same column, select the strongest
+    % peak among them and remove others
+    if length(col) > length(unique(col))
+        layer_mat = zeros(size(radar.data_smooth));
+        layer_mat(layers_idx{i}) = peaks(layers_idx{i});
+        multi_idx = sum(logical(layer_mat))>1;
+        col_nums = 1:size(layer_mat, 2);
+        for k = col_nums(multi_idx)
+            k_col = zeros(size(layer_mat, 1), 1);
+            [~,k_max] = max(layer_mat(:,k));
+            k_col(k_max) = layer_mat(k_max,k);
+            layer_mat(:,k) = k_col;
+        end
+        layers_idx{i} = find(layer_mat);
+    end
+
+    % Integrate peak magnitudes across ith layer to obtain layer
+    % prominence-distance value (accounting for lateral size of stacked
+    % radar trace bins)
 %     layers_val(i) = sum(peaks(layers_idx{i}));
-    layers_val(i) = sum(peaks(layers_idx{i}))*mean(diff(radar.dist));
-    layer_peaks(layers_idx{i}) = layers_val(i);
+    layers_val = sum(peaks(layers_idx{i}))*mean(diff(radar.dist));
+    
+    % Map layer prominence-distance values to the location within the radar
+    % matrix of the ith layer
+    layer_peaks(layers_idx{i}) = layers_val;
 end
 
+% Output layer arrays to radar structure
 radar.layers = layers_idx;
 radar.layer_vals = layer_peaks;
 
-P_50 = 2*1000;
-% P_50 = 2*1000/mean(iqr(radar.data_smooth));
-Po = 0.05;
+% P_50 = 2*1000;
+P_50 = 2*1000*mean(iqr(radar.data_smooth));
+Po = 0.001;
 K = 1;
 r = log((K*Po/0.50-Po)/(K-Po))/-P_50;
 
 ages = zeros([size(radar.data_smooth) Ndraw]);
-
+radar.likelihood = zeros(size(radar.data_smooth));
 for i = 1:size(layer_peaks, 2)
     
     peaks_i = layer_peaks(:,i);
@@ -187,6 +218,7 @@ for i = 1:size(layer_peaks, 2)
     % Probability of peak representing a year based on a logistic function
     % with rate (r) calculated above
     likelihood = K*Po./(Po + (K-Po)*exp(-r*peaks_i)); % times the prominence at that location?
+    radar.likelihood(peaks_idx,i) = likelihood;
     
     yr_idx = zeros(length(depths_i), Ndraw);
     for j = 1:length(depths_i)
