51f244a694fd0271de1b99781478f365f53a28f5
Durban Keeler <u1046484@ad.csbs.utah.edu>
2018-04-03 16:50:57 -0600

    Modified to P_50 to 1km times mean std of radar.data_smooth

--- radar_age.m
+++ radar_age.m
@@ -199,7 +199,9 @@ radar.layers = layers_idx;
 radar.layer_vals = layer_peaks;
 
 % P_50 = 2*1000;
-P_50 = 2*1000*mean(iqr(radar.data_smooth));
+% P_50 = 1000*2*mean(iqr(radar.data_smooth));
+P_50 = 1000*1*mean(std(radar.data_smooth));
+
 Po = 0.001;
 K = 1;
 r = log((K*Po/0.50-Po)/(K-Po))/-P_50;
