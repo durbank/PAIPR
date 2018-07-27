ee4796a2fe50e9f6313c2b96109cb05647a32fef
Durban Keeler <u1046484@ad.csbs.utah.edu>
2018-07-27 15:13:39 -0600

    Started script to explore the effect of different P_50 values to the estimated age-depth scales

---
+++ P50_compare.m
@@ -0,0 +1,49 @@
+% Script to compare the effect of differences in the assigned value of P_50
+
+% Directories to data of interest based on computer (eventually will be
+% replaced with GUI for data directory selection)
+PC_true = ispc;
+switch PC_true
+    case true
+        computer = 'work';
+        %         computer = input('Current PC: ');
+        switch computer
+            case 'work'
+                data_path = 'E:/Research/Antarctica/Data/';
+                addon_path = 'C:/Users/u1046484/Documents/MATLAB/Addons/';
+                
+            case 'laptop'
+                data_path = 'C:/Users/durba/Documents/Research/Antarctica/Data/';
+                addon_path = 'C:/Users/durba/Documents/MATLAB/Addons/';
+        end
+        
+    case false
+        data_path = '/media/durbank/WARP/Research/Antarctica/Data/';
+        addon_path = '/home/durbank/MATLAB/Add-Ons/';
+end
+
+% Addons needed for analysis
+% Add Antarctic Mapping Toolbox (AMT) to path
+addon_folder = fullfile(addon_path, 'AntarcticMappingTools_v5.03/');
+addpath(genpath(addon_folder))
+% Add export_fig to path
+addon_folder = fullfile(addon_path, 'altmany-export_fig-cafc7c5/');
+addpath(genpath(addon_folder))
+
+%%
+
+% Load core data from file (data used was previously generated using
+% import_cores.m)
+core_file = fullfile(data_path, 'Ice-cores/SEAT_cores/SEAT_cores.mat');
+cores = load(core_file);
+
+Ndraw = 100;
+
+% Load previously processed full OIB radar transect
+radar_full = load(fullfile(data_path, 'radar/SEAT_Traverses/results_data/OIB_SEAT10_4to10_6.mat'));
+
+%%
+
+%%% Need to reprocess full OIB data in order to include radar.groups field
+%%% (from these I can reconstruct what I need to in order to experiment
+%%% with P_50 values
