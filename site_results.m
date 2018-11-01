f2d0148c986dc78c00b284f7b01bbc8555c7902d
Durban G Keeler <durban.keeler@gmail.com>
2018-11-01 09:10:21 -0600

    Created new script 'site_results.m' for comparing optimzed results from the three core sites

---
+++ site_results.m
@@ -0,0 +1,138 @@
+% Script compare results from auto-IceBridge to cores and manual IceBridge
+
+% Directories to data of interest based on computer (eventually will be
+% replaced with GUI for data directory selection)
+PC_true = ispc;
+switch PC_true
+    case true
+        computer = 'laptop';
+        %         computer = input('Current PC: ');
+        switch computer
+            case 'work'
+                data_path = 'E:/Research/Antarctica/Data/';
+                addon_path = 'C:/Users/u1046484/Documents/MATLAB/Addons/';
+                
+            case 'laptop'
+                %                 data_path = 'C:/Users/durba/Documents/Research/Antarctica/Data/';
+                data_path = 'F:/Research/Antarctica/Data/';
+                addon_path = 'C:/Users/durba/Documents/MATLAB/Addons/';
+        end
+        
+    case false
+        data_path = '/media/durbank/WARP/Research/Antarctica/Data/';
+        addon_path = '/home/durbank/MATLAB/Add-Ons/';
+end
+
+% Addons needed for analysis
+% Add Antarctic Mapping Toolbox (AMT) to path
+addon_folder = fullfile(addon_path, 'AntarcticMappingTools_v5.03/');
+addpath(genpath(addon_folder))
+% Add export_fig to path
+addon_folder = fullfile(addon_path, 'altmany-export_fig-cafc7c5/');
+addpath(genpath(addon_folder))
+% Add CReSIS OIB MATLAB reader functions to path
+addon_folder = fullfile(addon_path, 'cresis-L1B-matlab-readers/');
+addpath(genpath(addon_folder))
+
+% Load core data from file (data used was previously generated using
+% import_cores.m)
+core_file = fullfile(data_path, 'Ice-cores/SEAT_cores/SEAT_cores.mat');
+cores = load(core_file);
+
+% Define number of Monte Carlo simulations to perform
+Ndraw = 100;
+
+%%
+
+% name = 'SEAT10_4';
+% input_dir = fullfile(data_path, 'IceBridge/manual_layers', name);
+% radar_ALL = radar_format(fullfile(input_dir, 'raw_data/'));
+% radar = radar_ALL(1).segment;
+% overlap = 10000;
+% horz_res = 25;
+% 
+% [radar_tmp] = radar_RT(radar, cores, Ndraw);
+% [radar_tmp] = calc_SWE(radar_tmp, Ndraw);
+% 
+% clip = round(0.5*overlap/horz_res);
+
+% fld_nm = fieldnames(radar_tmp);
+% fld_want = {'collect_date', 'Easting', 'Northing', 'dist', 'depth', ...
+%     'data_smooth', 'peaks', 'groups', 'ages', 'SMB_yr', 'SMB'};
+% 
+% radar = struct('collect_date', radar_tmp.collect_date, ...
+%     'Easting', radar_tmp.Easting(clip:end-clip),...
+%     'Northing', radar_tmp.Northing(clip:end-clip), ...
+%     'dist', radar_tmp.dist(clip:end-clip), 'depth', radar_tmp.depth, ...
+%     'data_smooth', radar_tmp.data_smooth(:,clip:end-clip),...
+%     'peaks', radar_tmp.peaks(:,clip:end-clip), ...
+%     'groups', radar_tmp.groups(:,clip:end-clip),...
+%     'likelihood', radar_tmp.likelihood(:,clip:end-clip), ...
+%     'ages', radar_tmp.ages(:,clip:end-clip,:));
+% radar.dist = radar.dist - radar.dist(1);
+% if isfield(radar_tmp, 'elev')
+%     radar.elev = radar_tmp.elev(clip:end-clip);
+% end
+% radar.SMB_yr =  radar_tmp.SMB_yr(clip:end-clip);
+% radar.SMB = radar_tmp.SMB(clip:end-clip);
+% 
+% fn = strcat('layers_', name, '.mat');
+% output_path = fullfile(input_dir, fn);
+% save(output_path, '-struct', 'radar', '-v7.3')
+
+
+%%
+
+% Name of SEAT core site to generate training data/perform regression
+name = 'SEAT10_6';
+
+% Load relevant radar data (previously generated using the above section)
+radar = load(fullfile(data_path, 'IceBridge/manual_layers', name, ...
+    strcat('layers_', name, '.mat')));
+
+% Load manually traced layers for current SEAT core site (generated using
+% 'draw_manual.m')
+tmp = load(fullfile(data_path, 'IceBridge/manual_layers', name, ...
+    strcat('manual_', name, '.mat')));
+man_layers = tmp.man_all;
+
+keep_idx = cellfun(@(x) round(x(:,2))<=size(radar.data_smooth,1), ...
+    man_layers, 'UniformOutput', false);
+man_layers = cellfun(@(x,y) x(y,:), man_layers, keep_idx, 'UniformOutput', false);
+
+%%
+
+man_mat = zeros(size(radar.data_smooth));
+
+for i = 1:length(man_layers)
+    
+    layer_idx = sub2ind(size(man_mat), round(man_layers{i}(:,2)), man_layers{i}(:,1));
+    man_mat(layer_idx) = i;
+end
+
+
+
+
+man_log = logical(man_mat);
+ages = zeros(size(radar.data_smooth));
+age_top = radar.collect_date;
+yr_pick1 = ceil(radar.collect_date - 1);
+
+for i = 1:size(ages,2)
+    depths_i = [0; radar.depth(man_log(:,i))];
+    yrs_i = ([age_top yr_pick1:-1:yr_pick1-length(depths_i)+2])';
+    ages(:,i) = interp1(depths_i, yrs_i, radar.depth, 'linear', 'extrap');
+end
+
+
+%%
+
+
+figure
+hold on
+plot(radar.depth, mean(ages,2), 'k')
+plot(radar.depth, mean(mean(radar.ages, 3), 2), 'm')
+plot(cores.(name).depth, mean(cores.(name).ages,2), 'b')
+
+
+    
