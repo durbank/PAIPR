6b09082a8debf99fbff45c2fa43834a6154ecdeb
Durban Keeler <u1046484@ad.csbs.utah.edu>
2018-11-15 12:39:55 -0700

    Updates to bring file import to compliance with new methods

--- DGK_fmcw_radar_serial_ku_plusgps.m
+++ DGK_fmcw_radar_serial_ku_plusgps.m
@@ -13,9 +13,50 @@
 % data_set  = 2;
 % start_num = 0;
 % stop_num = 240;
-radar_dir = ['/Volumes/WARP/Research/Antarctica/WAIS Variability' filesep...
-    'SEAT_Traverses/RawDataDurban/SEAT2011/'];
 
+%%
+
+% Directories to data of interest based on computer (eventually will be
+% replaced with GUI for data directory selection)
+PC_true = ispc;
+switch PC_true
+    case true
+        computer = 'work';
+        %         computer = input('Current PC: ');
+        switch computer
+            case 'work'
+                data_path = 'E:/Research/Antarctica/Data/';
+                addon_path = 'C:/Users/u1046484/Documents/MATLAB/Addons/';
+                
+            case 'laptop'
+                data_path = 'F:/Research/Antarctica/Data/';
+                addon_path = 'C:/Users/durba/Documents/MATLAB/Addons/';
+        end
+        
+    case false
+        data_path = '/media/durbank/WARP/Research/Antarctica/Data/';
+        addon_path = '/home/durbank/MATLAB/Add-Ons/';
+end
+
+% Addons needed for analysis
+% Add Antarctic Mapping Toolbox (AMT) to path
+addon_folder = fullfile(addon_path, 'AntarcticMappingTools_v5.03/');
+addpath(genpath(addon_folder))
+% Add export_fig to path
+addon_folder = fullfile(addon_path, 'altmany-export_fig-cafc7c5/');
+addpath(genpath(addon_folder))
+
+% Load core data from file (data used was previously generated using
+% import_cores.m)
+core_file = fullfile(data_path, 'Ice-cores/SEAT_cores/SEAT_cores.mat');
+cores = load(core_file);
+Ndraw = 100;
+
+
+radar_dir = fullfile(data_path, 'radar/SEAT_Traverses/RawDataDurban', ...
+    '/SEAT2011/');
+
+%%
 
 pulse_length = 250e-6;
 bandwidth = 2.6320e9;
@@ -25,7 +66,6 @@ bandwidth = 2.6320e9;
 
 
 % Output Directory
-out_dir = '/Volumes/WARP/Research/Antarctica/WAIS Variability/Data/radar-RAW/';
 % out_dir = '/icebridgedata/lorak/wais_2010/ku_band_12172010_out/';
 
 % Title for echogram
