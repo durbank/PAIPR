e242b6171d8a8bead35f03984f923bbe6dcadb20
Durban G Keeler <durban.keeler@gmail.com>
2019-05-03 20:49:02 -0600

    Modified scripts to convert Easting/Northing to longitude/latitude; saves final table to .csv file in selected directory

--- export_gam.m
+++ export_gam.m
@@ -8,10 +8,29 @@ data = load(fullfile(path, file));
 
 % output_dir = uigetdir('', 'Select directory to save converted tables');
 
+
+
+% Add Antarctic Mapping Toolbox (AMT) to path
+addon_path = '/home/durbank/MATLAB/Add-Ons/';
+addon_folder = fullfile(addon_path, 'AntarcticMappingTools_v5.03/');
+addpath(genpath(addon_folder))
+
+% Convert Northing, Easting to lat/lon
+[data.lat, data.lon] = ps2ll(data.Easting, data.Northing);
+
+
+
 table_cell = cellfun(@(a,b,c,d,e) accum_distGamma(a, b, c, d, e), ...
-    num2cell(data.Easting), num2cell(data.Northing), num2cell(data.elev), ...
+    num2cell(data.lat), num2cell(data.lon), num2cell(data.elev), ...
     data.SMB, data.SMB_yr, 'UniformOutput', false);
 long_table = vertcat(table_cell{:});
+
+
+output_dir = uigetdir('', 'Select directory to save converted tables');
+f_name = 'mat_table.csv';
+writetable(long_table, fullfile(output_dir, f_name));
+
+
 
 
 
