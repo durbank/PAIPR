f1880581b459cabc48ad66ce3adcd2eca929596b
Durban G. Keeler <durban.keeler@gmail.com>
2019-10-23 18:40:30 -0600

    Updates toolbox loading; cleans script for improved readability

--- PAIPR-core/PAIPR_script.m
+++ PAIPR-core/PAIPR_script.m
@@ -15,14 +15,14 @@ end
 
 % Addons needed for analysis
 % Add Antarctic Mapping Toolbox (AMT) to path
-addon_folder = fullfile(addon_path, 'AntarcticMappingTools_v5.03/');
-addpath(genpath(addon_folder))
+addon_struct = dir(fullfile(addon_path, 'AntarcticMappingTools_*'));
+addpath(genpath(fullfile(addon_struct.folder, addon_struct.name)))
 % Add export_fig to path
-addon_folder = fullfile(addon_path, 'altmany-export_fig-cafc7c5/');
-addpath(genpath(addon_folder))
+addon_struct = dir(fullfile(addon_path, 'altmany-export_fig*'));
+addpath(genpath(fullfile(addon_struct.folder, addon_struct.name)))
 % Add CReSIS OIB MATLAB reader functions to path
-addon_folder = fullfile(addon_path, 'cresis-L1B-matlab-readers/');
-addpath(genpath(addon_folder))
+addon_struct = dir(fullfile(addon_path, 'cresis-L1B-matlab-reader*'));
+addpath(genpath(fullfile(addon_struct.folder, addon_struct.name)))
 
 % Load core data from file (data used was previously generated using
 % import_cores.m)
@@ -36,9 +36,6 @@ Ndraw = 100;
 % 'data' directory path)
 radar_dir = uigetdir(data_path, ...
     'Select folder containing .mat radar data files');
-% radar_dir = fullfile(data_path, 'radar/SEAT_Traverses/SEAT2010Kuband/', ...
-%     'SEAT10_4toSEAT10_6');
-% radar_dir = fullfile(data_path, 'IceBridge/SEAT10_4to10_6/2011_SNO');
 
 % Define path to output directory of where to store results
 output_dir = uigetdir(data_path, ...
@@ -85,9 +82,9 @@ parfor i = 1:length(radar_ALL)
     
 %     % May be used in future versions of code
 %     fld_nm = fieldnames(radar_tmp);
-%     fld_want = {'collect_date', 'Easting', 'Northing', 'dist', 'depth', ...
-%         'rho_coeff', 'rho_var', 'data_smooth', 'peaks', 'groups', 'ages', ...
-%         'SMB_yr', 'SMB'};
+%     fld_want = {'collect_date', 'Easting', 'Northing', 'dist', ...
+%         'depth', 'rho_coeff', 'rho_var', 'data_smooth', 'peaks',...
+%         'groups', 'ages', 'SMB_yr', 'SMB'};
     
     % Clip radar data structure variables based on the desired radargram
     % overlap, and combine desired clipped variables into new data
@@ -96,7 +93,8 @@ parfor i = 1:length(radar_ALL)
     radar = struct('collect_date', radar_tmp.collect_date, ...
         'Easting', radar_tmp.Easting(clip:end-clip),...
         'Northing', radar_tmp.Northing(clip:end-clip), ...
-        'dist', radar_tmp.dist(clip:end-clip), 'depth', radar_tmp.depth, ...
+        'dist', radar_tmp.dist(clip:end-clip), ...
+        'depth', radar_tmp.depth, ...
         'rho_coeff', radar_tmp.rho_coeff(:,clip:end-clip), ...
         'rho_var', radar_tmp.rho_var(:,clip:end-clip),...
         'data_smooth', radar_tmp.data_smooth(:,clip:end-clip),...
@@ -113,6 +111,13 @@ parfor i = 1:length(radar_ALL)
     if isfield(radar_tmp, 'elev')
         radar.elev = radar_tmp.elev(clip:end-clip);
     end
+    
+    % Find layer member indices based on new clipped record
+    layers = cell(1, max(radar.groups(:)));
+    for j = 1:length(layers)
+        layers{j} = find(radar.groups == j);
+    end
+    radar.layers = layers(~cellfun(@isempty, layers));
     
     % Clip SMB cell array data and add to output struct
     radar.SMB_yr =  radar_tmp.SMB_yr(clip:end-clip);
@@ -155,7 +160,8 @@ parfor i = 1:length(radar_ALL)
         'Northing', radar_tmp.Northing(end-2*clip+1:end), ...
         'groups', radar_tmp.groups(:,end-2*clip+1:end),...
         'likelihood', radar_tmp.likelihood(:,end-2*clip+1:end), ...
-        'depth',radar_tmp.depth, 'ages',radar_tmp.ages(:,end-2*clip+1:end,:));
+        'depth',radar_tmp.depth, ...
+        'ages',radar_tmp.ages(:,end-2*clip+1:end,:));
     clip_end.SMB_yr = radar_tmp.SMB_yr(end-2*clip+1:end);
     clip_end.SMB = radar_tmp.SMB(end-2*clip+1:end);
     
