abb2980f40a1afd16a64c99a14fc5b0083c3a3f8
Durban G. Keeler <durban.keeler@gmail.com>
2019-11-04 20:05:51 -0700

    Subsets the raw modeled density and moves outside parfor loop for increased processing speed/reduced memory

--- PAIPR-core/PAIPR_script.m
+++ PAIPR-core/PAIPR_script.m
@@ -1,6 +1,9 @@
 % Wrapper function to import and process an arbitrary number of radargram
 % files within a single directory
+
 tic
+t = zeros(1,4);
+
 % Directories to data of interest based on computer (eventually will be
 % replaced with GUI for data directory selection)
 PC_true = ispc;
@@ -24,11 +27,6 @@ addpath(genpath(fullfile(addon_struct.folder, addon_struct.name)))
 addon_struct = dir(fullfile(addon_path, 'cresis-L1B-matlab-reader*'));
 addpath(genpath(fullfile(addon_struct.folder, addon_struct.name)))
 
-% Load core data from file (data used was previously generated using
-% import_cores.m)
-core_file = fullfile(data_path, 'Ice-cores/SEAT_cores/SEAT_cores.mat');
-cores = load(core_file);
-
 % Define number of Monte Carlo simulations to perform
 Ndraw = 100;
 
@@ -43,6 +41,26 @@ output_dir = uigetdir(data_path, ...
 gamma_dir = uigetdir(data_path, ...
     "Select folder to store output gamma-fitted .csv results");
 
+t(1) = toc;
+%%
+
+% % Load core data from file (data used was previously generated using
+% % import_cores.m)
+% core_file = fullfile(data_path, 'Ice-cores/SEAT_cores/SEAT_cores.mat');
+% cores = load(core_file);
+
+% Select modeled density file to use, and load in as a table
+[rho_fn, rho_path] = uigetfile([data_path '*.csv'], ...
+    "Select modeled density .csv file");
+rho_file = fullfile(rho_path, rho_fn);
+rho_raw = readtable(rho_file);
+
+% Convert modelled density lat/lon to Easting/Northing
+[rho_E, rho_N] = ll2ps(rho_raw{:,'Lat'}, rho_raw{:,'Lon'});
+T_locs = table(rho_E, rho_N,'VariableNames', {'Easting', 'Northing'});
+rho_full = [T_locs rho_raw(:,3:end)];
+clear rho_raw rho_E rho_N T_locs
+t(2) = toc;
 %%
 
 % Combine all input echograms and decompose into echogram subdomains of
@@ -66,19 +84,45 @@ end
 % Only keep data segments of sufficient length
 radar_ALL = radar_ALL(keep_idx);
 
+t(3) = toc();
+%%
+
+depth = 30;         % Depth of modeled density data
+depth_sz = 0.02;    % Vertical resolution of modeled data
+raw_res = 25;       % Horizontal resolution of modeled data
+new_res = 1000;     % Desired horizontal resolution of density data
+depth_num = round(depth/depth_sz);  % Num of observations at each point
+
+% Indices of starting (surface) locations to select
+select_idx = 1:(new_res/raw_res)*(depth_num+1):size(rho_full,1);
+
+% Preallocate table for density subset data
+rho_subset = table(0, 0, 'VariableNames', {'Easting', 'Northing'});
+rho_subset = repmat(rho_subset, length(select_idx), 1);
+rho_subset.Data = cell(height(rho_subset),1);
+
+for i = 1:height(rho_subset)
+    
+    % Add subset data to new table from original table
+    rho_subset.Easting(i) = rho_full.Easting(select_idx(i));
+    rho_subset.Northing(i) = rho_full.Northing(select_idx(i));
+    rho_subset.Data{i} = rho_full(...
+        select_idx(i):(select_idx(i)+depth_num),3:5); 
+end
+
+clear rho_full
+%%
+
 % Parellel for loop to process all decomposed echograms
-for i = 1:length(radar_ALL)
-    toc
-    % Find the mean response with depth in the radar data attributes across a
-    % given horizontal resolution (in meters)
+parfor i = 1:length(radar_ALL)
+    
+    % Find the mean response with depth in the radar data attributes across
+    % a given horizontal resolution (in meters)
     [radar_tmp] = radar_stack(radar_ALL(i).segment, horz_res);
     
     % Load modeled depth-density data from stats model output at specified
     % Easting/Northing coordinates
-    [rho_fn, rho_path] = uigetfile();
-    rho_file = fullfile(rho_path, rho_fn);
-    tic
-    [rho_data] = load_rho(rho_file, radar_tmp.Easting, radar_tmp.Northing);
+    [rho_data] = load_rho(rho_subset, radar_tmp.Easting, radar_tmp.Northing);
     
     % Convert to depth
     [radar_tmp] = radar_depth(radar_tmp, rho_data);
@@ -140,41 +184,43 @@ for i = 1:length(radar_ALL)
     
     %% Output overlapping data for comparisons of neighboring echograms
     
-    % Check for existence of directory for clipped results in output_dir,
-    % and create it if it does not exist
-    clip_dir = "result_clips";
-    if ~exist(fullfile(output_dir, clip_dir), 'dir')
-        mkdir(fullfile(output_dir, "result_clips"));
-    end
-    
-    % Keep relevant clipped variables from start of echogram for comparions
-    % of overlapping results
-    clip_start = struct('Easting', radar_tmp.Easting(1:2*clip), ...
-        'Northing', radar_tmp.Northing(1:2*clip), ...
-        'groups', radar_tmp.groups(:,1:2*clip),...
-        'likelihood', radar_tmp.likelihood(:,1:2*clip), ...
-        'depth',radar_tmp.depth, 'ages',radar_tmp.ages(:,1:2*clip,:));
-    clip_start.SMB_yr = radar_tmp.SMB_yr(1:2*clip);
-    clip_start.SMB = radar_tmp.SMB(1:2*clip);
-    
-    % Generate file names/paths for starting clip output and save
-    fn_start = sprintf('%s%d%s','clip_start',i, '.mat');
-    start_path = fullfile(output_dir, clip_dir, fn_start);
-    [save_success2] = parsave(clip_start, start_path);
-    
-    % Keep relevant clipped variables from end of echogram for comparions
-    % of overlapping results
-    clip_end = struct('Easting', radar_tmp.Easting(end-2*clip+1:end), ...
-        'Northing', radar_tmp.Northing(end-2*clip+1:end), 'groups', ...
-        radar_tmp.groups(:,end-2*clip+1:end), 'likelihood', ...
-        radar_tmp.likelihood(:,end-2*clip+1:end), 'depth', ...
-        radar_tmp.depth, 'ages', radar_tmp.ages(:,end-2*clip+1:end,:));
-    clip_end.SMB_yr = radar_tmp.SMB_yr(end-2*clip+1:end);
-    clip_end.SMB = radar_tmp.SMB(end-2*clip+1:end);
-    
-    % Generate file names/paths for end clip output and save
-    fn_end = sprintf('%s%d%s','clip_end',i, '.mat');
-    end_path = fullfile(output_dir, clip_dir, fn_end);
-    [save_success3] = parsave(clip_end, end_path);
+%     % Check for existence of directory for clipped results in output_dir,
+%     % and create it if it does not exist
+%     clip_dir = "result_clips";
+%     if ~exist(fullfile(output_dir, clip_dir), 'dir')
+%         mkdir(fullfile(output_dir, "result_clips"));
+%     end
+%     
+%     % Keep relevant clipped variables from start of echogram for comparions
+%     % of overlapping results
+%     clip_start = struct('Easting', radar_tmp.Easting(1:2*clip), ...
+%         'Northing', radar_tmp.Northing(1:2*clip), ...
+%         'groups', radar_tmp.groups(:,1:2*clip),...
+%         'likelihood', radar_tmp.likelihood(:,1:2*clip), ...
+%         'depth',radar_tmp.depth, 'ages',radar_tmp.ages(:,1:2*clip,:));
+%     clip_start.SMB_yr = radar_tmp.SMB_yr(1:2*clip);
+%     clip_start.SMB = radar_tmp.SMB(1:2*clip);
+%     
+%     % Generate file names/paths for starting clip output and save
+%     fn_start = sprintf('%s%d%s','clip_start',i, '.mat');
+%     start_path = fullfile(output_dir, clip_dir, fn_start);
+%     [save_success2] = parsave(clip_start, start_path);
+%     
+%     % Keep relevant clipped variables from end of echogram for comparions
+%     % of overlapping results
+%     clip_end = struct('Easting', radar_tmp.Easting(end-2*clip+1:end), ...
+%         'Northing', radar_tmp.Northing(end-2*clip+1:end), 'groups', ...
+%         radar_tmp.groups(:,end-2*clip+1:end), 'likelihood', ...
+%         radar_tmp.likelihood(:,end-2*clip+1:end), 'depth', ...
+%         radar_tmp.depth, 'ages', radar_tmp.ages(:,end-2*clip+1:end,:));
+%     clip_end.SMB_yr = radar_tmp.SMB_yr(end-2*clip+1:end);
+%     clip_end.SMB = radar_tmp.SMB(end-2*clip+1:end);
+%     
+%     % Generate file names/paths for end clip output and save
+%     fn_end = sprintf('%s%d%s','clip_end',i, '.mat');
+%     end_path = fullfile(output_dir, clip_dir, fn_end);
+%     [save_success3] = parsave(clip_end, end_path);
     
 end
+
+t(4) = toc;
--- PAIPR-core/load_rho.m
+++ PAIPR-core/load_rho.m
@@ -4,45 +4,45 @@
 % requisite R script to extract the density curves at the specified
 % echogram locations using the previously-optimized stats model)
 
-function [trace_rho] = load_rho(rho_file, Easting, Northing)
-
-% Read in density data from external file
-rho_mod = readtable(rho_file);
-
-% Convert Easting/Northing to lat/lon;
-[E, N] = ll2ps(rho_mod{:,'Lat'}, rho_mod{:,'Lon'});
-T_locs = table(E,N,'VariableNames', {'Easting', 'Northing'});
-rho_convert = [T_locs rho_mod(:,3:end)];
-
-% Find unique locations within loaded density file
-rho_loc = unique(rho_convert(:,{'Easting', 'Northing'}));
+function [trace_rho] = load_rho(rho_nest, Easting, Northing)
 
 % Find nearest depth-density profile to each trace in echogram
 near_idx = zeros(1, length(Easting));
 for i = 1:length(Easting)
     
-    dist = (Easting(i) - rho_loc.Easting).^2 + ...
-        (Northing(i) - rho_loc.Northing).^2;
+    dist = (Easting(i) - rho_nest.Easting).^2 + ...
+        (Northing(i) - rho_nest.Northing).^2;
     [~, near_idx(i)] = min(dist);
         
 end
-trace_rho = rho_loc(near_idx,:);
-
-
-% Preallocate cell array for depth-density data at each location
-trace_rho.Data = cell(height(trace_rho),1);
-
-% Add depth-density data to table for each location
-for i = 1:height(trace_rho)
-    
-    data_idx = rho_convert.Easting == trace_rho.Easting(i) & ...
-        rho_convert.Northing == trace_rho.Northing(i);
-    trace_rho.Data{i} = rho_convert(data_idx,3:end);
-end
+trace_rho = rho_nest(near_idx,:);
 
 
 
-
-
+% % Find unique locations within loaded density file
+% rho_loc = unique(rho_full_data(:,{'Easting', 'Northing'}));
+% 
+% % Find nearest depth-density profile to each trace in echogram
+% near_idx = zeros(1, length(Easting));
+% for i = 1:length(Easting)
+%     
+%     dist = (Easting(i) - rho_loc.Easting).^2 + ...
+%         (Northing(i) - rho_loc.Northing).^2;
+%     [~, near_idx(i)] = min(dist);
+%         
+% end
+% trace_rho = rho_loc(near_idx,:);
+% 
+% 
+% % Preallocate cell array for depth-density data at each location
+% trace_rho.Data = cell(height(trace_rho),1);
+% 
+% % Add depth-density data to table for each location
+% for i = 1:height(trace_rho)
+%     
+%     data_idx = rho_full_data.Easting == trace_rho.Easting(i) & ...
+%         rho_full_data.Northing == trace_rho.Northing(i);
+%     trace_rho.Data{i} = rho_full_data(data_idx,3:end);
+% end
 
 end
