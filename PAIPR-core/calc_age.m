8719513ac391a8048d6c812887910fc531c0bae7
Durban G. Keeler <durban.keeler@gmail.com>
2020-03-04 17:06:27 -0700

    Simplifies stationarizing using differencing

--- PAIPR-core/calc_age.m
+++ PAIPR-core/calc_age.m
@@ -4,31 +4,38 @@ function [radar] = calc_age(radar, r, k, Ndraw)
 
 %% Signal-noise processing
 
-% Stationarize the radar response by differencing traces with a smoothing
-% spline
-s = zeros(size(radar.data_stack));
-for i = 1:size(s, 2)
-    s(:,i) = csaps(radar.depth(:,i), radar.data_stack(:,i), ...
-        0.95, radar.depth(:,i));
-end
-radar_stat = radar.data_stack - s;
+% % Stationarize the radar response by differencing traces with a smoothing
+% % spline
+% s = zeros(size(radar.data_stack));
+% for i = 1:size(s, 2)
+%     s(:,i) = csaps(radar.depth(:,i), radar.data_stack(:,i), ...
+%         0.95, radar.depth(:,i));
+% end
+% radar_stat = radar.data_stack - s;
+% 
+% % Remove linear trend in variance (attentuation with depth) and convert to
+% % standardized values (z-score statistics)
+% radar_Z = zeros(size(radar_stat));
+% for i = 1:size(radar_stat, 2)
+%     data_i = radar_stat(:,i);
+%     % Frame length to define local variance
+%     half_frame = round(0.5*length(data_i)/5);
+%     var0 = movvar(data_i, 2*half_frame, 'EndPoints', 'discard');
+%     x = (half_frame:length(data_i)-half_frame)';
+%     % Linear trend in variance
+%     EQ = polyfit(x, var0, 1);
+%     x_mod = (1:length(data_i))';
+%     mod = polyval(EQ, x_mod);
+%     % Standardize variance-corrected data
+%     radar_Z(:,i) = data_i./sqrt(abs(mod));
+% end
 
-% Remove linear trend in variance (attentuation with depth) and convert to
-% standardized values (z-score statistics)
-radar_Z = zeros(size(radar_stat));
-for i = 1:size(radar_stat, 2)
-    data_i = radar_stat(:,i);
-    % Frame length to define local variance
-    half_frame = round(0.5*length(data_i)/5);
-    var0 = movvar(data_i, 2*half_frame, 'EndPoints', 'discard');
-    x = (half_frame:length(data_i)-half_frame)';
-    % Linear trend in variance
-    EQ = polyfit(x, var0, 1);
-    x_mod = (1:length(data_i))';
-    mod = polyval(EQ, x_mod);
-    % Standardize variance-corrected data
-    radar_Z(:,i) = data_i./sqrt(abs(mod));
-end
+
+
+% More straightforward stationarization using differencing
+radar_Z = [zeros(1, size(radar.data_stack,2)); ...
+    zscore(diff(radar.data_stack))];
+
 
 % Define the vertical resolution of the core data and horizontal resolution
 % of the radar data
