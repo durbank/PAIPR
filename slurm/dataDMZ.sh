1d82041730094d84a52f4427c5925f8499a10bea
Durban Keeler <u1046484@frisco1.wasatch.peaks>
2020-05-15 16:35:07 -0600

    Adds bash script to download rclone data in DMZ

---
+++ slurm/dataDMZ.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Script to create scratch directory and download data to it from gcloud
+# This should be implemented in the Science DMZ nodes
+
+# Purge old modules and load required ones
+module purge
+module load rclone
+
+# Define location of input data directories
+DATADIR="gcloud:CHPC/IceBridge-raw/wais-central-min/"
+
+# Define and create scratch directory
+SCRDIR="/scratch/general/lustre/u1046484/"
+mkdir -p $SCRDIR
+
+# Transfer input data to scratch
+rclone copy $DATADIR $SCRDIR/Data/ --transfers=16 --drive-chunk-size=32768
