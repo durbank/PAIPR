1ca480da475145fdcfb7edf7e089cc9dc4a475cd
Durban G. Keeler <durban.keeler@gmail.com>
2020-05-21 16:30:03 -0600

    Adds switch case for whether to overlap echograms; adds script for processing PAIPR locally

--- src/scripts/process_SLURM.m
+++ src/scripts/process_SLURM.m
@@ -4,10 +4,10 @@
 %               functions, and 'AntarcticMappingTools' functions to be
 %               added to the path previously
 
-function [success_codes] = process_SLURM(DATADIR, RHO_PATH, OUTDIR)
+function [success_codes] = process_SLURM(DATADIR, RHO_PATH, OUTDIR, Ndraw)
 
-% Define number of Monte Carlo simulations to perform
-Ndraw = 100;
+% % Define number of Monte Carlo simulations to perform
+% Ndraw = 100;
 
 % Directory containing raw OIB echograms
 radar_dir = DATADIR;
@@ -39,8 +39,9 @@ rho_table = import_rho(RHO_PATH);
 
 % Define overlap distance and the final horizontal resolution of the output
 % data
-overlap = 10000;
 horz_res = 25;
+overlap_switch = false;
+overlap = 10000;
 
 
 echo_0 = orderfields(import_radar(...
@@ -74,16 +75,17 @@ parfor i=1:length(end_idx)
         [radar_tmp] = radar_stack(echo_i, horz_res);
         
         
-        % Load modeled depth-density data from stats model output at specified
-        % Easting/Northing coordinates
-        [rho_data] = load_rho(rho_table, radar_tmp.Easting, radar_tmp.Northing);
+        % Load modeled depth-density data from stats model output at 
+        % specified Easting/Northing coordinates
+        [rho_data] = load_rho(...
+            rho_table, radar_tmp.Easting, radar_tmp.Northing);
         
         % Convert to depth
         [radar_tmp] = radar_depth(radar_tmp, rho_data);
         
-        % Calculate radar age-depth profile distributions (includes processing
-        %signal-noise, radon transforms, layer tracing, likelihood assignments,
-        % and age calculations)
+        % Calculate radar age-depth profile distributions (includes 
+        % processing signal-noise, radon transforms, layer tracing, 
+        % likelihood assignments, and age calculations)
         [radar_tmp] = calc_layers(radar_tmp, 'stream');
         
         % Calculate age-depth profiles
@@ -98,49 +100,63 @@ parfor i=1:length(end_idx)
         [QC_med, QC_val, QC_flag, depth_idx] = QC_check(...
             radar_tmp.IM_QC, 0.50, 0.10);
         
-        % Clip radar data structure variables based on the desired radargram
-        % overlap, and combine desired clipped variables into new data
-        % structure
-        clip = round(0.5*overlap/horz_res);
-        radar = struct('collect_time',radar_tmp.collect_time(clip:end-clip),...
-            'Easting', radar_tmp.Easting(clip:end-clip),...
-            'Northing', radar_tmp.Northing(clip:end-clip), ...
-            'dist', radar_tmp.dist(clip:end-clip), 'depth', radar_tmp.depth, ...
-            'data_smooth', radar_tmp.data_smooth(:,clip:end-clip),...
-            'IM_grad', radar_tmp.IM_grad(:,clip:end-clip), ...
-            'IM_QC', radar_tmp.IM_QC(:,clip:end-clip), 'QC_med', QC_med, ...
-            'QC_val', QC_val, 'QC_flag', QC_flag', 'QC_depth_idx', depth_idx,...
-            'DB', radar_tmp.DB(:,clip:end-clip), ...
-            'likelihood', radar_tmp.likelihood(:,clip:end-clip), ...
-            'ages', radar_tmp.ages(:,clip:end-clip,:));
-        
-        
-        if isfield(radar_tmp, 'groups')
-            radar.groups = radar_tmp.groups(:,clip:end-clip);
-            
-            % Find layer member indices based on new clipped record
-            layers = cell(1, max(radar.groups(:)));
-            for j = 1:length(layers)
-                layers{j} = find(radar.groups == j);
-            end
-            radar.layers = layers(~cellfun(@isempty, layers));
-            
+        switch overlap_switch
+            case true
+                % Clip radar data structure variables based on the desired 
+                % radargram %overlap, and combine desired clipped variables 
+                % into new data structure
+                clip = round(0.5*overlap/horz_res);
+                radar = struct(...
+                    'collect_time',radar_tmp.collect_time(clip:end-clip),...
+                    'Easting', radar_tmp.Easting(clip:end-clip),...
+                    'Northing', radar_tmp.Northing(clip:end-clip), ...
+                    'dist', radar_tmp.dist(clip:end-clip), ...
+                    'depth', radar_tmp.depth, ...
+                    'data_smooth', radar_tmp.data_smooth(:,clip:end-clip),...
+                    'IM_grad', radar_tmp.IM_grad(:,clip:end-clip), ...
+                    'IM_QC', radar_tmp.IM_QC(:,clip:end-clip), ...
+                    'QC_med', QC_med, 'QC_val', QC_val, ...
+                    'QC_flag', QC_flag', 'QC_depth_idx', depth_idx,...
+                    'DB', radar_tmp.DB(:,clip:end-clip), ...
+                    'likelihood', radar_tmp.likelihood(:,clip:end-clip), ...
+                    'ages', radar_tmp.ages(:,clip:end-clip,:));
+                
+                
+                if isfield(radar_tmp, 'groups')
+                    radar.groups = radar_tmp.groups(:,clip:end-clip);
+                    
+                    % Find layer member indices based on new clipped record
+                    layers = cell(1, max(radar.groups(:)));
+                    for j = 1:length(layers)
+                        layers{j} = find(radar.groups == j);
+                    end
+                    radar.layers = layers(~cellfun(@isempty, layers));
+                    
+                end
+                
+                % Check for existing elevation data, and add to output 
+                % struct if available
+                if isfield(radar_tmp, 'elev')
+                    radar.elev = radar_tmp.elev(clip:end-clip);
+                else
+                    radar.elev = nan(1, length(radar.Easting));
+                end
+                
+                % Redefine radargram distances based on clipped data
+                radar.dist = radar.dist - radar.dist(1);
+                
+                % Clip SMB cell array data and add to output struct
+                radar.SMB_yr =  radar_tmp.SMB_yr(clip:end-clip);
+                radar.SMB = radar_tmp.SMB(clip:end-clip);
+                
+            case false
+                
+                radar = radar_tmp;
+                radar.QC_med = QC_med;
+                radar.QC_val = QC_val;
+                radar.QC_flag = QC_flag;
+                radar.QC_depth_idx = depth_idx;
         end
-        
-        % Check for existing elevation data, and add to output struct if
-        % available
-        if isfield(radar_tmp, 'elev')
-            radar.elev = radar_tmp.elev(clip:end-clip);
-        else
-            radar.elev = nan(1, length(radar.Easting));
-        end
-        
-        % Redefine radargram distances based on clipped data
-        radar.dist = radar.dist - radar.dist(1);
-        
-        % Clip SMB cell array data and add to output struct
-        radar.SMB_yr =  radar_tmp.SMB_yr(clip:end-clip);
-        radar.SMB = radar_tmp.SMB(clip:end-clip);
         
         % Generate file names and paths under which to save data
         filename = sprintf('%s%d','radar_out',i);
@@ -151,8 +167,8 @@ parfor i=1:length(end_idx)
         [success_codes(i)] = parsave_all(radar, mat_output, csv_output);
         
     catch ME
-        fprintf(1, "An error occurred while processing data in the following directory: %s \n", ...
-            files(i).folder);
+        fprintf(1, ['An error occurred while processing data in the '...
+        'following directory: \n %s \n'], files(i).folder);
         fprintf(1, "Affected echograms span %s to %s \n", ...
             files(start_idx(i)).name, files(start_idx(i)).name)
         fn_missing = sprintf('%s%d', 'radar_out', i);
---
+++ src/scripts/process_local.m
@@ -0,0 +1,31 @@
+% Script to run PAIPR analyses locally (as opposed to HPC via SLURM batch
+% scripts)
+
+%% Add dependencies
+
+ROOT_DIR = fileparts(fileparts(pwd));
+
+% Add PAIPR-core functions to path
+addon_struct = dir(fullfile(ROOT_DIR, 'src/', 'PAIPR-core*'));
+addpath(genpath(fullfile(addon_struct.folder, addon_struct.name)))
+
+% Add cresis-matlab-reader functions to path
+addon_struct = dir(fullfile(ROOT_DIR, 'src/Dependencies/', ...
+    'cresis-L1B-matlab-reader*'));
+addpath(genpath(fullfile(addon_struct.folder, addon_struct.name)))
+
+% Add Antarctic Mapping Toolbox (AMT) to path
+addon_struct = dir(fullfile(ROOT_DIR, 'src/Dependencies/', ...
+    'AntarcticMappingTools_*'));
+addpath(genpath(fullfile(addon_struct.folder, addon_struct.name)))
+
+%% 
+
+DATADIR = ['/media/durbank/WARP/Research/Antarctica/Data/IceBridge/'...
+    'WAIS-central/20111109/'];
+RHOFILE = ['/media/durbank/WARP/Research/Antarctica/Data/rho-stats/Data/'...
+    'cent_interior/Data/densities/rho_20111109.csv'];
+OUTDIR = '/home/durbank/Documents/scratch/';
+NSIM = 100;
+
+[success_codes] = process_SLURM(DATADIR, RHOFILE, OUTDIR, NSIM);
