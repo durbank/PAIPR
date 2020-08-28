52a3eee9a707012b9d19070121ab2ac61f1f8bf6
Durban Keeler <u1046484@lonepeak2.wasatch.peaks>
2020-08-27 19:39:19 -0600

    Adds bash script to upload all files/results to gcloud

---
+++ slurm/upload_results.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Template script for uploading PAIPR-generated results to gcloud
+
+# Assign remote directory to which to upload
+CLOUD_DIR="gcloud:CHPC/PAIPR-results"
+
+# Assign path to source directory
+SRC_DIR=$PWD
+
+# Get name of enclosing direcotry
+dir_name="$(basename $SRC_DIR)"
+
+# Load the rclone module
+module load rclone
+
+# Define start time (for clocking execution speed)
+t_start=`date +%s`
+
+# Transfer sbatch source files and results to gcloud
+echo "Transferring files to gcloud..."
+rclone copy $SRC_DIR $CLOUD_DIR/$dir_name --transfers=8
+echo "Tranfer complete"
+
+# Define end time and calculate execution time
+t_end=`date +%s`
+runtime=$((t_end-t_start))
+echo "Total execution time:"
+echo $runtime
+
